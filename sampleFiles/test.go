3d61f2428fde2332b92abf206b823e87b1593df7
Erel Ozen <erelozen@gmail.com>
2026-02-20 16:28:54 -0800

    Extend Go structs with address and geometry methods

--- sampleFiles/test.go
+++ sampleFiles/test.go
@@ -17,8 +17,34 @@ func main() {
 // class
 
 type Person struct {
-	name string
-	age  int
+	name    string
+	age     int
+	email   string
+	address Address
+}
+
+type Address struct {
+	street string
+	city   string
+	state  string
+	zip    string
+}
+
+func NewPerson(name string, age int, email string) *Person {
+	return &Person{
+		name:  name,
+		age:   age,
+		email: email,
+	}
+}
+
+func (p *Person) SetAddress(street, city, state, zip string) {
+	p.address = Address{
+		street: street,
+		city:   city,
+		state:  state,
+		zip:    zip,
+	}
 }
 
 // function
@@ -54,4 +80,24 @@ func (p Person) sayHello() {
 type Rectangle struct {
 	width  float64
 	height float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (r Rectangle) perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) area() float64 {
+	return 3.14159 * c.radius * c.radius
+}
+
+func (c Circle) perimeter() float64 {
+	return 2 * 3.14159 * c.radius
 }
